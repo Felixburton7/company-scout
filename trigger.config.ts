00e5c37bf73e09956e533370e85c3191a56fb55c
Felix Burton <felixburton2002@gmail.com>
2025-12-26 21:33:56 +0000

    fix: add maxDuration to trigger config

--- trigger.config.ts
+++ trigger.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from "@trigger.dev/sdk/v3";
 
 export default defineConfig({
-    project: "proj_placeholder",
-    triggerDirectories: ["src/trigger"],
+  project: "proj_placeholder",
+  // Your Next.js project is configured with a 300s (5min) maxDuration by default.
+  // You can mistakenly override this in your `vercel.json` or in the Vercel dashboard.
+  maxDuration: 300, 
+  triggerDirectories: ["src/trigger"],
 });
